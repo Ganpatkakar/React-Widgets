1e8046985fc81eb7f08398f33e31275732f837ed
Ganpatkakar <kakarganpat@gmail.com>
2022-01-26 14:08:02 +0530

    Deploy Next.js to gh-pages

---
+++ _next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/home/datatable":["static\u002Fchunks\u002Fpages\u002Fhome\u002Fdatatable.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fhome\u002Fdatatable"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
