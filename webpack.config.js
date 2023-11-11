9082fd50dca2c0c65e3d3db8f64fea15fa7eb264
Ganpat kakar <kakarganpat@gmail.com>
2023-11-11 10:56:18 -0800

    Support tsx

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "/src/index.js", // main js
+  entry: "/src/index.tsx", // main js
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, "dist"), // output folder
