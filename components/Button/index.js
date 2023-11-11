cdaeeda4f08e99d044bc2c9e86495f65f115388e
Ganpat kakar <kakarganpat@gmail.com>
2023-11-10 22:00:58 -0800

    setup sectionbutton component

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './index.module.scss';
 
-export default function index({text, onClick: handleClick}) {
+export default function ({text, onClick: handleClick}) {
   return (
     <button className={styles.button} onClick={handleClick}>{text}</button>
   )
