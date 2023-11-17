3b41af5fe79501dd252ce9f8490106533a4cb4eb
Ganpat kakar <kakarganpat@gmail.com>
2023-11-17 05:35:33 -0800

    WySwigy Edtor component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
 import React from "react";
-import {
-  Accordian,
-  AccordianItem,
-  AccordianHeader,
-  AccordianPanel
-} from './components/Accordian';
+import EditorComponent from "./components/Editor";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React..!</h1>
-      <Accordian>
-        <AccordianItem value="1">
-          <AccordianHeader>Accordian 1</AccordianHeader>
-          <AccordianPanel><div>Accordian content of 1</div></AccordianPanel>
-        </AccordianItem>
-
-        <AccordianItem value="2">
-          <AccordianHeader>Accordian 2</AccordianHeader>
-          <AccordianPanel><div>Accordian content of 2</div></AccordianPanel>
-        </AccordianItem>
-
-        <AccordianItem value="3">
-          <AccordianHeader>Accordian 3</AccordianHeader>
-          <AccordianPanel><div>Accordian content of 3</div></AccordianPanel>
-        </AccordianItem>
-      </Accordian>
+      <EditorComponent />
     </div>
   );
 }
---
+++ src/components/Editor/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Editor from './Editor.component';
+
+export default function EditorComponent() {
+
+  return (
+    <>
+      <div>
+        <Editor containerClass={"editorContainer"} />
+      </div>
+    </>
+  )
+}
