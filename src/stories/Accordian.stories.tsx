38918f877aa81f002bb958af50ec5fd0d0a8dd4f
Ganpat kakar <kakarganpat@gmail.com>
2023-11-17 05:42:15 -0800

    Accordina component story

--- src/stories/Accordian.stories.tsx
+++ src/stories/Accordian.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
 import {
   Accordian,
   AccordianItem,
@@ -6,101 +7,96 @@ import {
   AccordianPanel
 } from '../components/Accordian';
 
-export default {
-  title: 'Components/Accordion',
-  component: Accordian,
-  argTypes: {},
-};
-
-const AccordionTemplate = ({}) => (
-  <Accordian>
-    <AccordianItem value="1">
-      <AccordianHeader>Accordian 1</AccordianHeader>
-      <AccordianPanel><div>Accordian content of 1</div></AccordianPanel>
-    </AccordianItem>
-
-    <AccordianItem value="2">
-      <AccordianHeader>Accordian 2</AccordianHeader>
-      <AccordianPanel><div>Accordian content of 2</div></AccordianPanel>
-    </AccordianItem>
-
-    <AccordianItem value="3">
-      <AccordianHeader>Accordian 3</AccordianHeader>
-      <AccordianPanel><div>Accordian content of 3</div></AccordianPanel>
-    </AccordianItem>
-  </Accordian>
-);
-
-export const Accordion = AccordionTemplate.bind({});
-
-const example = (
-  <Accordian>
-    <AccordianItem value="1">
-      <AccordianHeader>Accordian 1</AccordianHeader>
-      <AccordianPanel><div>Accordian content of 1</div></AccordianPanel>
-    </AccordianItem>
-
-    <AccordianItem value="2">
-      <AccordianHeader>Accordian 2</AccordianHeader>
-      <AccordianPanel><div>Accordian content of 2</div></AccordianPanel>
-    </AccordianItem>
-
-    <AccordianItem value="3">
-      <AccordianHeader>Accordian 3</AccordianHeader>
-      <AccordianPanel><div>Accordian content of 3</div></AccordianPanel>
-    </AccordianItem>
-  </Accordian>
-);
-
-Accordion.parameters = {
-  docs: {
-    source: {
-      code: example,
-    },
-  },
-};
-
-// import type { Meta, StoryObj } from '@storybook/react';
-
-// import { Accordian, AccordianHeader, AccordianItem, AccordianPanel } from '../components/Accordian';
-// import React from 'react';
-
-// const meta = {
-//   title: 'Accordian',
+// export default {
+//   title: 'Accordion',
 //   component: Accordian,
-//   parameters: {
-//     layout: 'fullScreen',
-//   },
-//   tags: ['autodocs'],
 //   argTypes: {},
-// } satisfies Meta<typeof Accordian>;
+// };
 
-// export default meta;
-// type Story = StoryObj<typeof meta>;
+// const AccordionTemplate = ({}) => (
+//   <Accordian>
+//     <AccordianItem value="1">
+//       <AccordianHeader>Accordian 1</AccordianHeader>
+//       <AccordianPanel><div>Accordian content of 1</div></AccordianPanel>
+//     </AccordianItem>
 
-// export const Accodrian: Story = {
-//   args: {
-//     children: [
-//       (
-//         <AccordianItem value="1">
-//           <AccordianHeader>Accordian 1</AccordianHeader>
-//           <AccordianPanel><div>Accordian content of 1</div></AccordianPanel>
-//         </AccordianItem>
-//       ),
+//     <AccordianItem value="2">
+//       <AccordianHeader>Accordian 2</AccordianHeader>
+//       <AccordianPanel><div>Accordian content of 2</div></AccordianPanel>
+//     </AccordianItem>
 
-//       (
-//         <AccordianItem value="2">
-//           <AccordianHeader>Accordian 2</AccordianHeader>
-//           <AccordianPanel><div>Accordian content of 2</div></AccordianPanel>
-//         </AccordianItem>
-//       ),
+//     <AccordianItem value="3">
+//       <AccordianHeader>Accordian 3</AccordianHeader>
+//       <AccordianPanel><div>Accordian content of 3</div></AccordianPanel>
+//     </AccordianItem>
+//   </Accordian>
+// );
 
-//       (
-//         <AccordianItem value="3">
-//           <AccordianHeader>Accordian 3</AccordianHeader>
-//           <AccordianPanel><div>Accordian content of 3</div></AccordianPanel>
-//         </AccordianItem>
-//       )
-//     ]
+// export const Accordion = AccordionTemplate.bind({});
+
+// const example = (
+//   <Accordian>
+//     <AccordianItem value="1">
+//       <AccordianHeader>Accordian 1</AccordianHeader>
+//       <AccordianPanel><div>Accordian content of 1</div></AccordianPanel>
+//     </AccordianItem>
+
+//     <AccordianItem value="2">
+//       <AccordianHeader>Accordian 2</AccordianHeader>
+//       <AccordianPanel><div>Accordian content of 2</div></AccordianPanel>
+//     </AccordianItem>
+
+//     <AccordianItem value="3">
+//       <AccordianHeader>Accordian 3</AccordianHeader>
+//       <AccordianPanel><div>Accordian content of 3</div></AccordianPanel>
+//     </AccordianItem>
+//   </Accordian>
+// );
+
+// Accordion.parameters = {
+//   docs: {
+//     source: {
+//       code: example,
+//     },
 //   },
 // };
+
+const meta = {
+  title: 'Accordian',
+  component: Accordian,
+  parameters: {
+    layout: 'fullScreen',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof Accordian>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Accodrian: Story = {
+  args: {
+    children: [
+      (
+        <AccordianItem value="1">
+          <AccordianHeader>Accordian 1</AccordianHeader>
+          <AccordianPanel><div>Accordian content of 1</div></AccordianPanel>
+        </AccordianItem>
+      ),
+
+      (
+        <AccordianItem value="2">
+          <AccordianHeader>Accordian 2</AccordianHeader>
+          <AccordianPanel><div>Accordian content of 2</div></AccordianPanel>
+        </AccordianItem>
+      ),
+
+      (
+        <AccordianItem value="3">
+          <AccordianHeader>Accordian 3</AccordianHeader>
+          <AccordianPanel><div>Accordian content of 3</div></AccordianPanel>
+        </AccordianItem>
+      )
+    ]
+  },
+};
