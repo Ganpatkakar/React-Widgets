12167d6dad46a215f26cf2742481a6854b610324
Ganpat kakar <kakarganpat@gmail.com>
2023-11-19 09:08:52 -0800

    Code preview purpose implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { SnakeGame } from "./components/Snake";
+import { Tab, TabList } from "./components/Tabs";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState('');
+
+  const handleSelectedTab = (value: string) => {
+    setSelectedTab(value);
+  }
+
   return (
     <div className="App">
       <h1>Hello React..!</h1>
-      <SnakeGame width={300} height={300}/>
+      <TabList onTabClick={handleSelectedTab}>
+        <Tab value='Tab1'>First Tab</Tab>
+        <Tab value='Tab2'>Second Tab</Tab>
+        <Tab value='Tab3'>Third Tab</Tab>
+      </TabList>
+      {
+        selectedTab === 'Tab1' && (
+          <>
+            The HyperText Markup Language or HTML is the standard markup language 
+            for documents designed to be displayed in a web browser.
+          </>
+        )
+      }
+      {
+        selectedTab === 'Tab2' && (
+          <>
+            Cascading Style Sheets is a style sheet language used for
+             describing the presentation of a document written in a markup language such as HTML or XML.
+          </>
+        )
+      }
+      {
+        selectedTab === 'Tab3' && (
+          <>
+            JavaScript, often abbreviated as JS, is a programming language 
+            that is one of the core technologies of the World Wide Web, alongside HTML and CSS.
+          </>
+        )
+      }
     </div>
   );
 }
