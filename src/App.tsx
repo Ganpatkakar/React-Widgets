7154941cb4113f485abdc3e48b51d9577df8a004
Ganpat kakar <kakarganpat@gmail.com>
2023-11-24 10:20:53 -0800

    Progress bar real life example

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,108 @@
-import React, { useReducer, useState } from "react";
-import Checkbox from "./components/Checkbox";
-import Slider from "./components/Slider";
+import React, { useEffect, useReducer, useRef, useState } from "react";
+import { ProgressBar } from "./components/Progressbar";
 
+// slider states => idle => 0%, loading => 95%, completed => 100%
+// when any api call happens change states
+// setLoading => this will modify the loading counts and modify value of progress bar based on states,
+// endLoading => modify => states based on current loading counts and modify the value
+
+const states = {
+  idle: 'idle',
+  loading: 'loading',
+  completed: 'completed'
+}
+let loading = 0;
 function App() {
-  const [slideVal, setSliderVal] = useState(10);
-  const handleSlider = (event, value) => {
-    setSliderVal(value);
-  };
+  const currentState = useRef(null); 
+  const [value, setValue] = useState(0);
+
+  const startLoading = () => {
+    if (loading === 0) {
+      currentState.current = states.idle
+    } else if (loading > 0) {
+      currentState.current = states.loading;
+    }
+
+    switch (currentState.current) {
+      case states.completed:
+        currentState.current = states.idle;
+        loading = 0;
+        setValue(0);
+      case states.idle:
+        currentState.current = states.loading;
+        setValue(0.75);
+        break;
+      case states.loading:
+        currentState.current = states.loading;
+        setValue(0.75);
+        break;
+    }
+    loading++;
+  }
+
+  const endLoading = () => {
+    loading = Math.max(loading - 1, 0);
+    if (loading === 0) {
+      currentState.current = states.completed
+    } else if (loading > 0) {
+      currentState.current = states.loading;
+    }
+
+    switch (currentState.current) {
+      case states.completed:
+        setValue(1);
+        break;
+      case states.loading:
+        setValue(0.75);
+        break;
+    }
+  }
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      startLoading();
+      const apiTimer = setTimeout(() => {
+        endLoading();
+        console.log("3 second timer");
+        clearTimeout(timer);
+        clearTimeout(apiTimer);
+
+        startLoading();
+        const apiTimer2 = setTimeout(() => {
+          endLoading();
+          console.log("5 second timer");
+          clearTimeout(timer);
+          clearTimeout(apiTimer2);
+        }, 5000);
+
+      }, 3000);
+
+      setTimeout(() =>{
+        startLoading();
+        const apiTimer1 = setTimeout(() => {
+          endLoading();
+          console.log("3 second timer");
+          clearTimeout(timer);
+          clearTimeout(apiTimer1);
+        }, 2000);
+
+        startLoading();
+        const apiTimer2 = setTimeout(() => {
+          endLoading();
+          console.log("4 second timer");
+          clearTimeout(timer);
+          clearTimeout(apiTimer2);
+        }, 4000);
+      }, 500)
+
+    }, 100);
+  }, []);
+
+
   return (
     <div className="App">
       <h1>Hello React..!</h1>
-      <Slider
-        defaultValue={20}
-        vertical={false}
-        value={slideVal}
-        handleSliderChange={handleSlider}
-        step={25}
-      />
+      <ProgressBar value={value} label="Api calls progress bar" />
     </div>
   );
 }
--- src/stories/ProgressBar.stories.tsx
+++ src/stories/ProgressBar.stories.tsx
@@ -97,3 +97,223 @@ ContinouseProgress.parameters = {
     },
   },
 };
+
+
+const states = {
+  idle: 'idle',
+  loading: 'loading',
+  completed: 'completed'
+}
+let loading = 0;
+
+function template2() {
+  const currentState = React.useRef(null); 
+  const [value, setValue] = React.useState(0);
+
+  const startLoading = () => {
+    if (loading === 0) {
+      currentState.current = states.idle
+    } else if (loading > 0) {
+      currentState.current = states.loading;
+    }
+
+    switch (currentState.current) {
+      case states.completed:
+        currentState.current = states.idle;
+        loading = 0;
+        setValue(0);
+      case states.idle:
+        currentState.current = states.loading;
+        setValue(0.75);
+        break;
+      case states.loading:
+        currentState.current = states.loading;
+        setValue(0.75);
+        break;
+    }
+    loading++;
+  }
+
+  const endLoading = () => {
+    loading = Math.max(loading - 1, 0);
+    if (loading === 0) {
+      currentState.current = states.completed
+    } else if (loading > 0) {
+      currentState.current = states.loading;
+    }
+
+    switch (currentState.current) {
+      case states.completed:
+        setValue(1);
+        break;
+      case states.loading:
+        setValue(0.75);
+        break;
+    }
+  }
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      startLoading();
+      const apiTimer1 = setTimeout(() => {
+        endLoading();
+        console.log("2 second timer");
+        clearTimeout(timer);
+        clearTimeout(apiTimer1);
+
+        startLoading();
+        const apiTimer2 = setTimeout(() => {
+          endLoading();
+          console.log("3 second timer");
+          clearTimeout(timer);
+          clearTimeout(apiTimer2);
+        }, 3000);
+
+      }, 2000);
+
+      setTimeout(() =>{
+        startLoading();
+        const apiTimer3 = setTimeout(() => {
+          endLoading();
+          console.log("1 second timer");
+          clearTimeout(timer);
+          clearTimeout(apiTimer3);
+        }, 1000);
+
+        startLoading();
+        const apiTimer4 = setTimeout(() => {
+          endLoading();
+          console.log("2 second timer");
+          clearTimeout(timer);
+          clearTimeout(apiTimer4);
+        }, 2000);
+      }, 500)
+
+    }, 100);
+  }, []);
+
+
+  return (
+    <ProgressBar value={value} label="Api calls progress bar" />
+  );
+}
+
+export const ApiCallUpdatesProgressBar = template2.bind({});
+
+const eample2 = `
+import React, { useEffect, useReducer, useRef, useState } from "react";
+import { ProgressBar } from "./components/Progressbar";
+
+// slider states => idle => 0%, loading => 95%, completed => 100%
+// when any api call happens change states
+// setLoading => this will modify the loading counts and modify value of progress bar based on states,
+// endLoading => modify => states based on current loading counts and modify the value
+
+const states = {
+  idle: 'idle',
+  loading: 'loading',
+  completed: 'completed'
+}
+let loading = 0;
+function App() {
+  const currentState = useRef(null); 
+  const [value, setValue] = useState(0);
+
+  const startLoading = () => {
+    if (loading === 0) {
+      currentState.current = states.idle
+    } else if (loading > 0) {
+      currentState.current = states.loading;
+    }
+
+    switch (currentState.current) {
+      case states.completed:
+        currentState.current = states.idle;
+        loading = 0;
+        setValue(0);
+      case states.idle:
+        currentState.current = states.loading;
+        setValue(0.75);
+        break;
+      case states.loading:
+        currentState.current = states.loading;
+        setValue(0.75);
+        break;
+    }
+    loading++;
+  }
+
+  const endLoading = () => {
+    loading = Math.max(loading - 1, 0);
+    if (loading === 0) {
+      currentState.current = states.completed
+    } else if (loading > 0) {
+      currentState.current = states.loading;
+    }
+
+    switch (currentState.current) {
+      case states.completed:
+        setValue(1);
+        break;
+      case states.loading:
+        setValue(0.75);
+        break;
+    }
+  }
+
+  // Replicate API call using set timeout, with multiple overlaping calls example
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      startLoading();
+      const apiTimer1 = setTimeout(() => {
+        endLoading();
+        console.log("2 second timer");
+        clearTimeout(timer);
+        clearTimeout(apiTimer1);
+
+        startLoading();
+        const apiTimer2 = setTimeout(() => {
+          endLoading();
+          console.log("3 second timer");
+          clearTimeout(timer);
+          clearTimeout(apiTimer2);
+        }, 3000);
+
+      }, 2000);
+
+      setTimeout(() =>{
+        startLoading();
+        const apiTimer3 = setTimeout(() => {
+          endLoading();
+          console.log("1 second timer");
+          clearTimeout(timer);
+          clearTimeout(apiTimer3);
+        }, 1000);
+
+        startLoading();
+        const apiTimer4 = setTimeout(() => {
+          endLoading();
+          console.log("2 second timer");
+          clearTimeout(timer);
+          clearTimeout(apiTimer4);
+        }, 2000);
+      }, 500)
+
+    }, 100);
+  }, []);
+
+
+  return (
+    <ProgressBar value={value} label="Api calls progress bar" />
+  );
+}
+
+`;
+
+ApiCallUpdatesProgressBar.parameters = {
+  docs: {
+    source: {
+      code: eample2,
+    },
+  },
+};
