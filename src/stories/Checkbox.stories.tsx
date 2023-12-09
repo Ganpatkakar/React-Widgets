8d2998579464756ddd54528e967507076165363a
Ganpat kakar <kakarganpat@gmail.com>
2023-12-09 12:27:24 -0800

    update checkbox folder name

--- src/stories/Checkbox.stories.tsx
+++ src/stories/Checkbox.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useState } from "react";
+import React, { useReducer } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { Checkbox } from "../components/CheckBox";
 
