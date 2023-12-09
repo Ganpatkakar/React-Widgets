f0af15dcc800b6e7751c72e760771b164f8f07fa
Ganpat kakar <kakarganpat@gmail.com>
2023-12-09 12:34:28 -0800

    update checkbox folder import export

--- src/stories/Checkbox.stories.tsx
+++ src/stories/Checkbox.stories.tsx
@@ -1,6 +1,6 @@
 import React, { useReducer } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
-import { Checkbox } from "../components/CheckBox";
+import { Checkbox } from "../components/CheckBox/checkbox";
 
 const meta = {
   title: "Components/Checkbox",
