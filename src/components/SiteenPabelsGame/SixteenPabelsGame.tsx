123b4be3ecf9b5d3fb25ab0597c21e9b02c8faed
Ganpat kakar <kakarganpat@gmail.com>
2026-05-17 02:41:21 +0530

    Game board and players rendered based on the gameMatrix

--- src/components/SiteenPabelsGame/SixteenPabelsGame.tsx
+++ src/components/SiteenPabelsGame/SixteenPabelsGame.tsx
@@ -40,11 +40,15 @@ export function SixteenPabelsGame() {
   const bottomTrianlgeCordinates = bottomTrianlgeCircles(lowerTriangleWidth, lowerTriangleHeight);
   const squareCordinates = squareCircles(squareWidth, squareHeight);
 
+  const handlePlayerClick = (a: number, b: number) => {
+    console.log("handlePlayerClick", a, b)
+  }
+
   return (
     <div className={styles.gameContainer}>
       <div style={{ width: '100%', }}>
-        <Stage width={containerWidth} height={upperTriangleHeight + 60}>
-          <Layer x={containerWidth / 4 + 30} y={60}>
+        <Stage width={containerWidth + 60} height={upperTriangleHeight + squareHeight + lowerTriangleHeight + 60}>
+          <Layer x={containerWidth / 4 + 30} y={30}>
             <Shape
               sceneFunc={(ctx, shape) => {
                 ctx.beginPath();
@@ -69,41 +73,37 @@ export function SixteenPabelsGame() {
               strokeWidth={1}
             />
             {
-              topTrianlgeCordinates.map((cordinate, index) => {
-                const { x, y, r = 30, a = 0, b = 0 } = cordinate;
-                let dice = null;
-                if (gameMatrix[a][b] !== 0) {
-                  const player = gameMatrix[a][b];
-                  let isCoinSelected = false;
-                  // if (state.sourceCoordinates !== null) {
-                  //   isCoinSelected = state.sourceCoordinates.a === a && state.sourceCoordinates.b === b;
-                  // }
-                  dice = (
-                    <PlayerPositionRender
-                      xAxis={x}
-                      yAxis={y}
-                      radius={isCoinSelected ? 15 : 10}
-                      player={players[player - 1]}
-                    />
-                  )
-                }
-                return (
-                  <div key={index}>
-                    <Circle
-                      key={index}
-                      x={x}
-                      y={y}
-                      radius={r}
-                    />
-                    {dice}
-                  </div>
-                );
-              })
+              renderPlayers(topTrianlgeCordinates, handlePlayerClick)
             }
           </Layer>
-        </Stage>
-        <Stage width={squareWidth + 60} height={squareHeight + 60}>
-          <Layer x={30} y={30}>
+          <Layer x={containerWidth / 4 + 30} y={upperTriangleHeight + squareHeight + 30}>
+            <Shape
+              sceneFunc={(ctx, shape) => {
+                ctx.beginPath();
+
+                // Triange shape
+                ctx.moveTo(lowerTriangleWidth / 2, 0);
+                ctx.lineTo(0, lowerTriangleHeight);
+                ctx.lineTo(lowerTriangleWidth, lowerTriangleHeight);
+                ctx.lineTo(lowerTriangleWidth / 2, 0);
+
+                // First line in triangle from top "lowerTriangleHeight / 3"
+                ctx.moveTo(lowerTriangleWidth / 4, lowerTriangleHeight / 2);
+                ctx.lineTo(lowerTriangleWidth - lowerTriangleWidth / 4, lowerTriangleHeight / 2);
+
+                ctx.moveTo(lowerTriangleWidth / 2, 0);
+                ctx.lineTo(lowerTriangleWidth / 2, lowerTriangleHeight);
+                ctx.closePath();
+                ctx.fillStrokeShape(shape);
+              }}
+              stroke="black"
+              strokeWidth={1}
+            />
+            {
+              renderPlayers(bottomTrianlgeCordinates, handlePlayerClick)
+            }
+          </Layer>
+          <Layer x={30} y={upperTriangleHeight + 30}>
             <Shape
               sceneFunc={(ctx, shape) => {
                 ctx.beginPath();
@@ -144,98 +144,44 @@ export function SixteenPabelsGame() {
               strokeWidth={1}
             />
             {
-              squareCordinates.map((cordinate, index) => {
-                const { x, y, r = 30, a = 0, b = 0 } = cordinate;
-                let dice = null;
-                if (gameMatrix[a][b] !== 0) {
-                  const player = gameMatrix[a][b];
-                  let isCoinSelected = false;
-                  // if (state.sourceCoordinates !== null) {
-                  //   isCoinSelected = state.sourceCoordinates.a === a && state.sourceCoordinates.b === b;
-                  // }
-                  dice = (
-                    <PlayerPositionRender
-                      xAxis={x}
-                      yAxis={y}
-                      radius={isCoinSelected ? 15 : 10}
-                      player={players[player - 1]}
-                    />
-                  )
-                }
-                return (
-                  <div key={index}>
-                    <Circle
-                      key={index}
-                      x={x}
-                      y={y}
-                      radius={r}
-                    />
-                    {dice}
-                  </div>
-                );
-              })
-            }
-          </Layer>
-        </Stage>
-        <Stage width={containerWidth} height={upperTriangleHeight + 30}>
-          <Layer x={containerWidth / 4 + 30} y={30}>
-            <Shape
-              sceneFunc={(ctx, shape) => {
-                ctx.beginPath();
-
-                // Triange shape
-                ctx.moveTo(lowerTriangleWidth / 2, 0);
-                ctx.lineTo(0, lowerTriangleHeight);
-                ctx.lineTo(lowerTriangleWidth, lowerTriangleHeight);
-                ctx.lineTo(lowerTriangleWidth / 2, 0);
-
-                // First line in triangle from top "lowerTriangleHeight / 3"
-                ctx.moveTo(lowerTriangleWidth / 4, lowerTriangleHeight / 2);
-                ctx.lineTo(lowerTriangleWidth - lowerTriangleWidth / 4, lowerTriangleHeight / 2);
-
-                ctx.moveTo(lowerTriangleWidth / 2, 0);
-                ctx.lineTo(lowerTriangleWidth / 2, lowerTriangleHeight);
-                ctx.closePath();
-                ctx.fillStrokeShape(shape);
-              }}
-              stroke="black"
-              strokeWidth={1}
-            />
-            {
-              bottomTrianlgeCordinates.map((cordinate, index) => {
-                const { x, y, r = 30, a = 0, b = 0 } = cordinate;
-                let dice = null;
-                if (gameMatrix[a][b] !== 0) {
-                  const player = gameMatrix[a][b];
-                  let isCoinSelected = false;
-                  // if (state.sourceCoordinates !== null) {
-                  //   isCoinSelected = state.sourceCoordinates.a === a && state.sourceCoordinates.b === b;
-                  // }
-                  dice = (
-                    <PlayerPositionRender
-                      xAxis={x}
-                      yAxis={y}
-                      radius={isCoinSelected ? 15 : 10}
-                      player={players[player - 1]}
-                    />
-                  )
-                }
-                return (
-                  <div key={index}>
-                    <Circle
-                      key={index}
-                      x={x}
-                      y={y}
-                      radius={r}
-                    />
-                    {dice}
-                  </div>
-                );
-              })
+              renderPlayers(squareCordinates, handlePlayerClick)
             }
           </Layer>
         </Stage>
       </div>
     </div>
   )
+}
+
+const renderPlayers = (coordinates: Array<any>, clickHandler: any) => {
+  return coordinates.map((cordinate, index) => {
+    const { x, y, r = 30, a = 0, b = 0 } = cordinate;
+    let dice = null;
+    if (gameMatrix[a][b] !== 0) {
+      const player = gameMatrix[a][b];
+      let isCoinSelected = false;
+      // if (state.sourceCoordinates !== null) {
+      //   isCoinSelected = state.sourceCoordinates.a === a && state.sourceCoordinates.b === b;
+      // }
+      dice = (
+        <PlayerPositionRender
+          xAxis={x}
+          yAxis={y}
+          radius={isCoinSelected ? 15 : 10}
+          player={players[player - 1]}
+        />
+      )
+    }
+    return (
+      <div key={index} onClick={() => clickHandler(a, b)}>
+        <Circle
+          key={index}
+          x={x}
+          y={y}
+          radius={r}
+        />
+        {dice}
+      </div>
+    );
+  })
 }
